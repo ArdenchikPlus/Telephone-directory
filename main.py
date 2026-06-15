75658d25759e82823955effaf0362784038e67c1
ArdenchikPlus <raskovalov132@gmail.com>
2026-06-15 10:59:09 +0300

    last version (Adding file saving and the ability to delete a contact)

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-contacts = {
-    "Ivan": "+79991112233",
-    "Anna": "+79994445566"
-}
+import json
+import os
+
+file_name = "contacts.json"
+
+if os.path.exists(file_name):
+    with open(file_name, "r", encoding="utf-8") as file:
+        contacts = json.load(file)
+else:
+    contacts = {
+        "Ivan": "+79991112233",
+        "Anna": "+79994445566"
+    }
+
 while True:
-    print("1. Find a contact.\n 2. Add a contact.\n 3. Show all.\n 4. Exit.")
+    print("1. Find a contact.\n2. Add a contact.\n3. Show all.\n4. Delete contact:\n5. Exit")
     action = int(input("Select action: "))
     if action == 1:
         name = input("Enter a name to search: ")
@@ -27,5 +37,18 @@ while True:
             print(f"{name}: {phone}")
 
     if action == 4:
+        name = input("Enter a name to delete: ")
+        name_check = name.title()
+
+        if name_check in contacts:
+            del contacts[name_check] #delete
+            print(f"Contact {name_check} successfully deleted!")
+        else:
+            print("Contact not found.")
+
+    if action == 5:
+        with open(file_name, "w", encoding="utf-8") as file:
+            json.dump(contacts, file, ensure_ascii=False, indent=4)
+        print("Contacts saved.")
         break
 
