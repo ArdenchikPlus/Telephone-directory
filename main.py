d4b06b67f61bd37eadb51dfe50b97ee894642a96
ArdenchikPlus <raskovalov132@gmail.com>
2026-06-15 10:22:24 +0300

    adding find a contact

---
+++ main.py
@@ -0,0 +1,14 @@
+contacts = {
+    "Ivan": "+79991112233",
+    "Anna": "+79994445566"
+}
+while True:
+    print("\n1. Find a contact.\n 2. Add a contact.\n 3. Show all.\n 4. Exit.")
+    action = int(input("Select action: "))
+    if action == 1:
+        if action == 1:
+            name = input("Enter a name to search: ")
+            if name in contacts:
+                print(f"Phone number: {contacts[name]}")
+            else:
+                print("Contact not found.")
