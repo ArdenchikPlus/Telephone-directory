3dcc1b7b7019b31fa3f10f38dd39378fa8637001
ArdenchikPlus <raskovalov132@gmail.com>
2026-06-15 10:45:31 +0300

    adding check

--- main.py
+++ main.py
@@ -3,23 +3,29 @@ contacts = {
     "Anna": "+79994445566"
 }
 while True:
-    print("\n1. Find a contact.\n 2. Add a contact.\n 3. Show all.\n 4. Exit.")
+    print("1. Find a contact.\n 2. Add a contact.\n 3. Show all.\n 4. Exit.")
     action = int(input("Select action: "))
     if action == 1:
-        if action == 1:
-            name = input("Enter a name to search: ")
-            if name in contacts:
-                print(f"Phone number: {contacts[name]}")
-            else:
-                print("Contact not found.")
+        name = input("Enter a name to search: ")
+
+        name_check = name.title() # check for capital letters
+
+        if name_check in contacts:
+            print(f"Phone number: {contacts[name_check]}")
+        else:
+            print("Contact not found.")
+
     if action == 2:
         name_user = input("Enter name:")
+        name_check = name_user.title()
         number = input("Enter number:")
-        contacts[name_user] = number
+        contacts[name_check] = number
         print("Successfully!")
+
     if action == 3:
         for name, phone in contacts.items():
             print(f"{name}: {phone}")
+
     if action == 4:
         break
 
