2e3c9ccf421d7206ba5dad83ca5dc119ad2da41a
ArdenchikPlus <raskovalov132@gmail.com>
2026-06-18 18:00:34 +0300

    creating a window (commit 2)

--- app.py
+++ app.py
@@ -8,4 +8,32 @@ app.title("Phonebook v1.0")
 app.geometry("500x600")
 app.resizable(False, False)
 
+title_label = ctk.CTkLabel(
+    master=app,
+    text="Telephone directory",
+    font=("Arial", 24, "bold")
+)
+title_label.pack(pady=20)
+
+search_entry = ctk.CTkEntry(
+    master=app,
+    placeholder_text="Enter a name or number to search...",
+    width=350,
+    height=40,
+    corner_radius=8
+)
+search_entry.pack(pady=10)
+
+search_button = ctk.CTkButton(
+    master=app,
+    text="Find a contact",
+    width=150,
+    height=40,
+    corner_radius=8,
+    font=("Arial", 14, "bold"),
+    fg_color="#1f6aa5",
+    hover_color="#144870"
+)
+search_button.pack(pady=10)
+
 app.mainloop()
